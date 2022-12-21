b3958180cc9404bbe2bda2884bb3a29ca6f46c5f
Vijay Pushkin <mail@vijaypushkin.me>
2022-12-21 19:29:49 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Component } from "solid-js";
+
+const App: Component = () => {
+  return (
+    <div class="flex flex-col gap-2">
+      Hello World
+    </div>
+  );
+};
+
+export default App;
